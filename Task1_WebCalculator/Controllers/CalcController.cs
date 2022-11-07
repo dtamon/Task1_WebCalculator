59668e6aa10efaef2d17d3dce359e3e2ab96cbae
dtamon <dominiktamon@gmail.com>
2022-11-07 09:03:33 +0100

    Model&View&Controller done

---
+++ Task1_WebCalculator/Controllers/CalcController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Task1_WebCalculator.Models;
+
+namespace Task1_WebCalculator.Controllers
+{
+    public class CalcController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(new Calculator());
+        }
+
+        [HttpPost]
+        public IActionResult Index(Calculator c, string operation)
+        {
+            if (operation == "add") { 
+                c.result = c.num1 + c.num2;
+            } 
+            else if (operation == "sub")
+            {
+                c.result = c.num1 - c.num2;
+            }
+            else if (operation == "mul")
+            {
+                c.result = c.num1 * c.num2;
+            }
+            else if (operation == "div")
+            {
+                c.result = c.num1 / c.num2;
+            }
+
+            return View(c);
+        }
+    }
+}
---
+++ Task1_WebCalculator/Models/Calculator.cs
@@ -0,0 +1,9 @@
+﻿namespace Task1_WebCalculator.Models
+{
+    public class Calculator
+    {
+        public double num1 { get; set; }
+        public double num2 { get; set; }
+        public double result { get; set; }
+    }
+}
